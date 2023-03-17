322de0074d01f39fa292caaacb5ec2fbeb302a31
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2023-03-17 19:03:18 +0500

    test again

--- components/context/UserProvider.tsx
+++ components/context/UserProvider.tsx
@@ -108,7 +108,7 @@ export const UserProvider : FC<ProviderType>  = (props: Props) => {
                 document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`
                 document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`
 
-                router.reload()
+                router.push('/signin')
 
                 setState((props)=>({  
                   ...props,
