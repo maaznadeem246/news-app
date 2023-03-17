0115135dd89611610d20070126e4f07b705a04af
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2023-03-17 17:46:17 +0500

    test again

--- components/context/UserProvider.tsx
+++ components/context/UserProvider.tsx
@@ -79,7 +79,7 @@ export const UserProvider : FC<ProviderType>  = (props: Props) => {
 
      }).catch((er)=>{
       //console.log(er)
-      router.push('/signin')
+    
       setState((props)=>({  
         ...props,
         accessToken:null,
@@ -108,7 +108,7 @@ export const UserProvider : FC<ProviderType>  = (props: Props) => {
                 document.cookie = `my-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`
                 document.cookie = `my-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`
 
-                router.push('/signin')
+                router.reload()
 
                 setState((props)=>({  
                   ...props,
