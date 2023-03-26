6ccc87da05bb0ee708f857bdb9e3461025ad48bf
Muhammad Maaz Uddin <maaznadeem246@gmail.com>
2023-03-26 02:24:49 -0700

    tset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -65,7 +65,7 @@ export default function App({ Component, pageProps,  emotionCache = clientSideEm
       </UserProvider>
      </SessionContextProvider>
      </CacheProvider>
-     <Analytics mode='production' />
+     <Analytics  />
      </>
      )
 }
